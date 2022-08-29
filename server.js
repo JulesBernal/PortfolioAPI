5e0d2b6a453ddc2e0d7af425bf78e1be2eba7484
JulesBernal <102987740+JulesBernal@users.noreply.github.com>
2022-08-29 03:22:39 -0400

    project api

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const info = require('./portfolio.json');
+
+console.log(info);
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.get('/*', (req, res) => {
+    res.sendFile(portfolio.json);
+});
